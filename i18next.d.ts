d9fb38869e3d3753f93d96f19607ef6c08909854
gaetan <andrianjafygaetan@gmail.com>
2022-12-17 10:46:11 +0300

    Fix key not compatible

--- i18next.d.ts
+++ i18next.d.ts
@@ -1,9 +1,7 @@
 import {Locales} from './src/locales';
-import {DEFAULT_NS} from './i18n';
 
 declare module 'i18next' {
   interface CustomTypeOptions {
-    defaultNS: typeof DEFAULT_NS;
     resources: typeof Locales['en'];
   }
 }
