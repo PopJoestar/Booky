6474fcea1ed0a688d2d741fad9f5931e54705573
gaetan <andrianjafygaetan@gmail.com>
2022-12-19 18:43:58 +0300

    Change Library screen header

--- src/features_library/components/Header.tsx
+++ src/features_library/components/Header.tsx
@@ -1,11 +1,9 @@
 import {useNavigation} from '@react-navigation/native';
 import React from 'react';
 import {useTranslation} from 'react-i18next';
-import {IconButton} from 'react-native-paper';
-import {Box, Icon, Row, Text, TouchableRipple} from '../../shared/components';
-import {useAppTheme} from '../../shared/hooks';
+import {Appbar} from 'react-native-paper';
+
 const Header = () => {
-  const {colors, sizes} = useAppTheme();
   const {t} = useTranslation();
 
   const navigation = useNavigation();
@@ -15,33 +13,11 @@ const Header = () => {
   };
 
   return (
-    <TouchableRipple
-      marginTop="searchbarHeader"
-      onPress={goToSearchScreen}
-      borderRadius="hg"
-      marginHorizontal="m"
-      marginBottom="s"
-      borderless>
-      <Row
-        alignItems="center"
-        height={sizes.searchbar}
-        paddingLeft="m"
-        backgroundColor="surfaceVariant">
-        <Icon
-          name="magnify"
-          size={sizes.l}
-          mr="m"
-          color={colors.onSurfaceVariant}
-        />
-        <Box flex={1}>
-          <Text textAlign="left" color="onSurfaceVariant" variant="bodyLarge">
-            {t('search')}
-          </Text>
-        </Box>
-
-        <IconButton icon="dots-vertical" size={sizes.l} onPress={() => {}} />
-      </Row>
-    </TouchableRipple>
+    <Appbar.Header>
+      <Appbar.Content title={t('library:title')} />
+      <Appbar.Action icon="magnify" onPress={goToSearchScreen} />
+      <Appbar.Action icon="filter-variant" onPress={() => {}} />
+    </Appbar.Header>
   );
 };
 
--- src/features_library/components/LibraryScreen.tsx
+++ src/features_library/components/LibraryScreen.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import LibraryTabs from './LibraryTabs';
+import BooksTab from './BooksTab';
 
 const LibraryScreen = () => {
   return (
     <>
-      <LibraryTabs />
+      <BooksTab />
     </>
   );
 };
