89bf9337d2f45436cb5349978a2a2a05a0f4010b
gaetan <andrianjafygaetan@gmail.com>
2023-02-12 19:25:28 +0300

    Add plugin resolver

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,15 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./'],
+        alias: {
+          '@': './src',
+        },
+      },
+    ],
     ['@babel/plugin-proposal-decorators', {legacy: true}],
     'react-native-reanimated/plugin',
   ],
