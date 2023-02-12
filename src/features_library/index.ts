68347eaeb2fcc621e049791e1f9c7146ec5a579e
gaetan <andrianjafygaetan@gmail.com>
2023-02-12 23:28:59 +0300

    Remove WatermelonDB

--- src/features_library/index.ts
+++ src/features_library/index.ts
@@ -1,16 +1,5 @@
 import CollectionsTab from './components/CollectionsTab';
 import Header from './components/Header';
 import LibraryScreen from './components/LibraryScreen';
-import Author from './model/Authors';
-import Book from './model/Book';
-import BookAuthor from './model/BookAuthor';
-import * as LibrarySchema from './model/schema';
-export {
-  LibraryScreen,
-  Header,
-  CollectionsTab,
-  Book,
-  Author,
-  BookAuthor,
-  LibrarySchema,
-};
+
+export {LibraryScreen, Header, CollectionsTab};
