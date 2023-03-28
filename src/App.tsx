455fbe1e79436760c2f1a7147a7baf999095fc45
gaetan <andrianjafygaetan@gmail.com>
2023-03-28 08:53:53 +0300

    Test m3 color

--- src/App.tsx
+++ src/App.tsx
@@ -8,17 +8,17 @@ import {Provider as PaperProvider} from 'react-native-paper';
 import {RealmProvider} from './db/database';
 import Navigations from './navigation/Navigations';
 import {SnackbarProvider} from './shared/components';
-import {LightTheme} from './theme';
+import {LightTheme, PaperTheme} from './theme';
 
 const App = () => {
   return (
-    <PaperProvider>
+    <PaperProvider theme={PaperTheme}>
       <ThemeProvider theme={LightTheme}>
         <SnackbarProvider>
           <GestureHandlerRootView style={styles.ghRoot}>
             <BottomSheetModalProvider>
               <RealmProvider fallback={<></>}>
-                <NavigationContainer theme={LightTheme}>
+                <NavigationContainer theme={PaperTheme}>
                   <StatusBar
                     backgroundColor={'transparent'}
                     barStyle="dark-content"
