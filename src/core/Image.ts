93dcbd062541cf3318fe14cf1765582839ee4138
gaetan <andrianjafygaetan@gmail.com>
2023-08-05 16:11:36 +0300

    feat(core): use expo-image

--- src/core/Image.ts
+++ src/core/Image.ts
@@ -1,8 +1,7 @@
 import {createBox} from '@shopify/restyle';
 import {Theme} from '@/theme';
-import {Image as RNIMage} from 'react-native';
-import {ImageProps} from 'react-native';
+import {ImageProps, Image as RNImage} from 'expo-image';
 
-const Image = createBox<Theme, ImageProps>(RNIMage);
+const Image = createBox<Theme, ImageProps>(RNImage);
 
 export default Image;
