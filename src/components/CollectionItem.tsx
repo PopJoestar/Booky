80037ff83f9b150a70c5c21d3e7b8a4040ae6f38
gaetan <andrianjafygaetan@gmail.com>
2023-08-05 16:38:12 +0300

    feat(components): add books images

--- src/components/CollectionItem.tsx
+++ src/components/CollectionItem.tsx
@@ -1,14 +1,18 @@
-import {Box, Surface, Text} from '@/core';
+import {Box, Image, Row, Surface, Text} from '@/core';
 import {CollectionModel} from '@/database';
 import {useAppTheme} from '@/hooks';
 import React from 'react';
+import {StyleSheet} from 'react-native';
 
 type Props = {
   item: CollectionModel;
 };
 
 const CollectionItem = ({item}: Props) => {
-  const {sizes} = useAppTheme();
+  const {sizes, spacing} = useAppTheme();
+  const bookImages = item.books.map(book => book.image);
+  const splicedBookImages = [...bookImages].splice(0, 3);
+
   return (
     <Surface
       padding={'s'}
@@ -25,6 +29,40 @@ const CollectionItem = ({item}: Props) => {
             <Text variant={'bodySmall'}>No item</Text>
           </Box>
         ) : null}
+        {item.books.length > 0 ? (
+          <Row
+            flex={1}
+            flexShrink={0}
+            flexWrap={'wrap'}
+            rowGap={'s'}
+            justifyContent={'space-between'}>
+            {splicedBookImages.map((image, index) => (
+              <Box key={index}>
+                <Image
+                  source={{uri: image}}
+                  height={70}
+                  width={(sizes.collection_item_width - 10 - spacing.s * 2) / 2}
+                  contentFit="contain"
+                  alignSelf={'flex-start'}
+                />
+              </Box>
+            ))}
+            {bookImages.length > 3 ? (
+              <Box
+                width={(sizes.collection_item_width - 10 - spacing.s * 2) / 2}
+                style={styles.fakeImage}
+                height={70}>
+                <Box
+                  backgroundColor={'surfaceContainerHighest'}
+                  flex={1}
+                  alignItems={'center'}
+                  justifyContent={'center'}>
+                  <Text numberOfLines={1}>{`+${bookImages.length - 3}`}</Text>
+                </Box>
+              </Box>
+            ) : null}
+          </Row>
+        ) : null}
       </Box>
       <Box flex={1}>
         <Text variant={'titleSmall'} textAlign={'center'} numberOfLines={2}>
@@ -34,5 +72,11 @@ const CollectionItem = ({item}: Props) => {
     </Surface>
   );
 };
+
+const styles = StyleSheet.create({
+  fakeImage: {
+    paddingHorizontal: 12,
+  },
+});
 
 export default CollectionItem;
