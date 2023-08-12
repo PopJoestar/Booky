a2448a6ce8d51e28b1467322007066366d170836
gaetan <andrianjafygaetan@gmail.com>
2023-08-12 19:13:54 +0300

    feat: add storage permission check on android < 11

--- src/utils/permissions.ts
+++ src/utils/permissions.ts
@@ -1,5 +1,6 @@
 import {startActivityAsync} from 'expo-intent-launcher';
 import ExternalStorage from 'externalStorage';
+import {PermissionsAndroid, Platform} from 'react-native';
 
 export const redirectToManageExternalStoragePermission = async () => {
   await startActivityAsync(
@@ -8,14 +9,34 @@ export const redirectToManageExternalStoragePermission = async () => {
 };
 
 export const requestExternalStoragePermission = async () => {
-  let isExternalStorageManager =
-    await ExternalStorage.isExternalStorageManager();
+  //  android 11+
+  if (parseInt(Platform.Version.toString(), 10) >= 30) {
+    let isExternalStorageManager =
+      await ExternalStorage.isExternalStorageManager();
 
-  if (!isExternalStorageManager) {
-    await redirectToManageExternalStoragePermission();
+    if (!isExternalStorageManager) {
+      await redirectToManageExternalStoragePermission();
 
-    isExternalStorageManager = await ExternalStorage.isExternalStorageManager();
+      isExternalStorageManager =
+        await ExternalStorage.isExternalStorageManager();
+    }
+    return isExternalStorageManager;
   }
 
-  return isExternalStorageManager;
+  const isGranted = await requestStoragePermission();
+
+  return isGranted;
+};
+
+const requestStoragePermission = async () => {
+  const result = await PermissionsAndroid.requestMultiple([
+    PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE,
+    PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
+  ]);
+
+  return (
+    result[PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE] ===
+      'granted' &&
+    result[PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE] === 'granted'
+  );
 };
