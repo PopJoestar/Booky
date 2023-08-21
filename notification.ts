b89943cb038ba59b9656448db2c5056d1677bc95
gaetan <andrianjafygaetan@gmail.com>
2023-08-21 19:00:33 +0300

    feat: add open book on notification pressed

--- notification.ts
+++ notification.ts
@@ -1,3 +1,18 @@
-import notifee from '@notifee/react-native';
+import {openFileWithThirdPartyApp} from '@/utils/files';
+import notifee, {Event, EventType} from '@notifee/react-native';
 
-notifee.onBackgroundEvent(async () => {});
+const openBookFile = async (e: Event) => {
+  if (
+    e.detail.notification?.data?.filePath === undefined ||
+    e.type !== EventType.PRESS
+  ) {
+    return;
+  }
+
+  try {
+    openFileWithThirdPartyApp(e.detail.notification.data.filePath as string);
+  } catch {}
+};
+
+notifee.onBackgroundEvent(openBookFile);
+notifee.onForegroundEvent(openBookFile);
